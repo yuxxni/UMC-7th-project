86caeae1e327e354b8535d63b400227deff133e6
Cho seo yun <yuxxni05@gmail.com>
2024-10-02 02:45:38 +0900

    Add files via upload

---
+++ week2/src/App.jsx
@@ -0,0 +1,82 @@
+import {useState} from 'react';
+import "./App.css";
+import Input from './components/Input';
+import Button from './components/Button';
+
+function App() {
+  const [ todos, setTodos] = useState([
+    {id: 1, task: '투두 만들어보기'},
+    {id: 2, task: '희연 혜원 혜윤 건 찬민'},
+
+  ]);
+
+
+const [text, setText] = useState('');
+const[editingId, setEditingId] = useState('');
+const [ editText, setEditText]= useState('');
+ 
+// 랜더링 방지
+const handleSubmit = (e) => {
+  e.preventDefault();
+};
+// 1. 추가하기
+const addTodo = () => {
+  setTodos((prev) => [
+    ...prev,
+    {id: Math.floor(Math.random() * 100) + 2, task: text},
+  ]);
+
+};
+
+// 2. 삭제하기
+const deleteTodo = (id) => {
+  setTodos((prev) => prev.filter((item) => item.id !== id));
+};
+// 3. 수정하기
+const updateTodo = (id, text) => {
+  setTodos((prev) => 
+    prev.map((item) => (item.id === id ? {...item, task:text}: item))
+);
+setEditingId('');
+};
+
+return (
+  <>
+    <form onSubmit={handleSubmit} className= 'form'>
+      <Input value={text} onChange={setText} />
+      <Button onClick={addTodo} label= "할 일 등록"/>
+      </form>
+      
+      <div>
+        {todos.map((todo, _) => (
+        <div key={todo.id} className= 'todoItem'>
+          {editingId !== todo.Id &&(
+        <div key={todo.id} className='content' >
+          <p>{todo.id}</p>
+          <p>{todo.task}</p>
+          </div>
+          )}
+          {editingId === todo.id &&(
+            <div key={todo.id} className='content'>
+            <p>{todo.id}</p>
+    
+            <Input value={todo.task} onChange={setEditText}/>
+            </div>
+
+
+          )}
+        
+        <Button onClick={() => deletTodo(todo.id)}label="삭제하기"/>
+        {editingId === todo.id ? (
+          <Button onClick={() => updateTodo(editingId, editText )} label="수정 완료"/>
+        ): (
+        <Button onClick={() => setEditingId(todo.id)} label="수정 진행" />
+        
+        )}
+        </div>
+        ))}
+      </div> 
+  </>
+);
+}
+export default App
---
+++ week2/src/assets/components/Button.jsx
@@ -0,0 +1,5 @@
+function Button({ onClick, label }) {
+ return  <button onClick={onClick}> 
+  {label} </button>;
+};
+export default Button;
