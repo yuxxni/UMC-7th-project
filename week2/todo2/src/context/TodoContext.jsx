87fbb68ca003edd30c59544b6b4011616209da04
yuxxni <yuxxni05@gmail.com>
2024-11-06 01:49:25 +0900

    Context-API 리팩토링

---
+++ week2/todo2/src/context/TodoContext.jsx
@@ -0,0 +1,62 @@
+import React, { createContext, useState } from 'react';
+
+export const TodoContext = createContext();
+
+export function TodoContextProvider({ children }) { 
+  const [todos, setTodos] = useState([]);
+  const [text, setText] = useState('');
+  const [editingId, setEditingId] = useState('');
+  const [editText, setEditText] = useState('');
+
+  // 랜더링 방지
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  // 1. 추가하기
+  const addTodo = () => {
+    setTodos((prev) => [
+      ...prev,
+      { id: Math.floor(Math.random() * 100) + 2, task: text },
+    ]);
+    setText(''); // 추가 후 입력창 초기화
+  };
+
+  // 2. 삭제하기
+  const deleteTodo = (id) => {
+    setTodos((prev) => prev.filter((item) => item.id !== id));
+  };
+
+  // 3. 수정하기
+  const updateTodo = (id, text) => {
+    setTodos((prev) =>
+      prev.map((item) => (item.id === id ? { ...item, task: text } : item))
+    );
+    setEditingId('');
+    setEditText(''); // 수정 완료 후 초기화
+  };
+
+  return (
+    <TodoContext.Provider
+      value={{
+        todos,
+        setTodos,
+        text,
+        setText,
+        editingId,
+        setEditingId,
+        editText,
+        setEditText,
+        handleSubmit,
+        addTodo,
+        deleteTodo,
+        updateTodo,
+      }}
+    >
+      
+        {children}
+ 
+    </TodoContext.Provider>
+  );
+}
+
